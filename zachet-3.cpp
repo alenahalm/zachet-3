7a78af1714b48a6df919eb33129b77bace42f955
alenahalm <alenahalmatova749@gmail.com>
2020-12-26 10:08:06 +0800

    Initial commit

---
+++ zachet-3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int n = 5;
+	int a[n];
+	cout << "Enter array" << endl;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		sum += a[i];
+		a[i] = sum;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+}
